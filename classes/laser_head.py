7a4ee1e6b5a0acb3b5cb5bb70906f6c01535a2a2
matijasukovic <suksa186@gmail.com>
2024-10-21 17:53:33 +0100

    feat: add function getTiltAngleLimits to LaserHead
    returns min and max angles the tilt servo can take based on pan position

--- classes/laser_head.py
+++ classes/laser_head.py
@@ -1,5 +1,6 @@
 from classes.servo import Servo
 from adafruit_servokit import ServoKit
+import math
 
 class LaserHead:
     def __init__(self):
@@ -11,6 +12,33 @@ class LaserHead:
     def reset(self):
         self.pan.mid()
         self.tilt.mid()
+
+
+
+    def getTiltAngleLimits(self):
+        p = abs(math.cos(2 * math.radians(self.pan.getAngle())))
+
+        minAngle_lowerLimit = 43
+        minAngle_upperLimit = 54
+
+        minAngle = minAngle_lowerLimit + (minAngle_upperLimit - minAngle_lowerLimit) * p
+
+        maxAngle_lowerLimit = 119
+        maxAngle_upperLimit = 125
+
+        maxAngle = maxAngle_lowerLimit + (maxAngle_upperLimit - maxAngle_lowerLimit) * (1 - p)
+
+        if (0 <= self.pan.getAngle() <= 38 and self.tilt.getAngle() > 90) or (139 <= self.pan.getAngle() <= 180 and self.tilt.getAngle() < 90):
+            # Pointing to the right side of the image (farther from the laser head), slightly increase limits
+            minAngle = minAngle - 3
+            maxAngle = maxAngle + 7 
+        elif (0 <= self.pan.getAngle() <= 38 and self.tilt.getAngle() < 90) or (139 <= self.pan.getAngle() <= 180 and self.tilt.getAngle() > 90):
+            # Pointing to the left side of the image (closer to laser head), decrease limits slightly
+            minAngle = minAngle + 6
+            maxAngle = maxAngle - 1
+
+        return minAngle, maxAngle
+
 
     def test_servoRange(self):
         from time import sleep
